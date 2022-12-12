53e526960360cd11b2548089c5d3e8c1840bc13a
M S I <M S I@MSI>
2022-12-12 16:37:18 +0700

    Add class program and int

--- Queues_122/Program.cs
+++ Queues_122/Program.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace Queues_122
 {
-
-    internal class Program
+    class Program
     {
+        int FRONT, REAR, max = 5;
+        int[] queue_array= new int[5];
+
+        public Program()
+        {
+            /*intializin the value of the variable RAEAR and FRONT to -1 to indicate that
+             * the queue is initially empty*/
+            FRONT = -1;
+            REAR = -1;
+        }
+
+        
         static void Main(string[] args)
         {
         }
