04de9c4ad7bca129ca6ced1271c6a0326e243334
M S I <M S I@MSI>
2022-12-12 16:44:08 +0700

    add public void insert and statement

--- Queues_122/Program.cs
+++ Queues_122/Program.cs
@@ -19,7 +19,18 @@ namespace Queues_122
             REAR = -1;
         }
 
-        
+        public void insert(int element)
+        {
+            /* this statement cheks for the over flow condition */
+            if ((FRONT == 0 && REAR == max -1 ) ||(FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            /* this following statement checks whether the queue is empty. if the queue is empty
+             * then the value of the REAR and FRONT variables is set to 0*/
+
+        }
         static void Main(string[] args)
         {
         }
